3c8472f531b7a09bd98ae3d11f664eb36cdb8f36
Victor Boleac <victorboleac@Victors-MacBook-Pro.local>
2024-11-22 07:34:52 +0200

    saving project

--- find_a_job/find_a_job.py
+++ find_a_job/find_a_job.py
@@ -12,6 +12,16 @@ import utilities.lyb_for_files as lf
 BASE_LINK = "https://findajob.dwp.gov.uk"
 for link in lb.build_link(BASE_LINK):
     print(link)
+    response = requests.get(link)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    job_list = soup.find_all('div', class_='search-result')
 
-
-
+    for job in job_list:
+        job_link = job.find('a', class_='govuk-link')['href']
+        respone = requests.get(job_link)
+        job_soup = BeautifulSoup(respone.text, 'html.parser')
+        job_title = job_soup.find('h1', class_='govuk-heading-l govuk-!-margin-top-8').text.strip()
+        print(job_title)
+        
+        
+        print(salary_row)
