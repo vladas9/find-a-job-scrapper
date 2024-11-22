f5f73e1f239ae44b16277b162ed460cf49c596a3
Victor Boleac <victorboleac@Victors-MacBook-Pro.local>
2024-11-22 05:14:06 +0200

    feat: added gitignore

---
+++ find_a_job.py
@@ -0,0 +1,3 @@
+import lyb_for_files as lyb
+import beautifulsoup4 as bs4
+import requests
--- read_input_files.py
+++
@@ -1,7 +0,0 @@
-def read_file(file_name):
-    with open(file_name, 'r') as f:
-        data = f.read()
-        word_list = [word.strip() for word in data.split(',')]
-    return word_list
-
-#print(readFile('keywords.txt'))
