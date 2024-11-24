f4ff91605cd7a189fb8d2688369fb6ba930dec46
Victor Boleac <victorboleac@Victors-MacBook-Pro.local>
2024-11-25 01:35:46 +0200

    feat: updated the logic of the link-utils for cvlibrary

--- utils/link_utils.py
+++ utils/link_utils.py
@@ -11,14 +11,26 @@ def build_link(base_link, keyword, city):
     Returns:
         str: a link to the job search
     """
-    return f"{base_link}/search?adv=1&qwd={keyword}&cat=28&f=1&w={city}&d=25&pp=25&sb=date&sd=down"
+    if base_link == 'find-a-job':
+        return f"{base_link}/search?adv=1&qwd={keyword}&cat=28&f=1&w={city}&d=25&pp=25&sb=date&sd=down"
+    elif base_link == 'cv-library':
+        return (f"{base_link}/{keyword.replace(' ', '-')}-jobs-in-{city}"
+                f"?categories=construction&distance=25&order=date&posted=1&us=1")
 
 def check_link(link):
-    job_list = cj("scraped_jobs/find_a_job.json")
+    json_path = ""
+
+    if link.startswith("https://www.cv-library.co.uk"):
+        json_path = "scraped_jobs/cv_library.json"
+    elif link.startswith("https://findajob.dwp.gov.uk") :
+        json_path = "scraped_jobs/find_a_job.json"
+
+    job_list = cj(json_path)
+
     if link in job_list:
         checker = False
     else:
         checker = True
         job_list.append(link)
-        aj(job_list, "scraped_jobs/find_a_job.json")
+        aj(job_list, json_path)
     return checker
