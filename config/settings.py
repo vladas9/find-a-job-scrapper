fc006ee6a02d9f47972b160dec3cdc3c4e10fe5e
vladas9 <vladamza63@gmail.com>
2025-04-03 18:20:08 +0300

    changed the script link in configs

--- config/settings.py
+++ config/settings.py
@@ -1,2 +1,2 @@
 BASE_LINK = "https://www.cv-library.co.uk"
-SPREADSHEET_URL = "https://script.google.com/macros/s/AKfycbzJ8ePGlm1RqvIGbSWtYEaRbdEW6rqI0WsyOvaOSyRvwz7YqRbmSxrsoVwI0yDNbtTT/exec"
+SPREADSHEET_URL = "https://script.google.com/macros/s/AKfycby6MtKgNk-4tn9e4z_4m4scXGS4utOBdbz6ItjcsUo-w1bvpHoQevfS3JYQ0qYwGC4/exec"
